17b47408c4dda3ea9043d7f11aba9ae139012346
Blaise Rodrigues <109841528+blaiserods26@users.noreply.github.com>
2024-04-30 17:46:00 +0530

    Update 3Dmap.py

--- 3Dmap.py
+++ 3Dmap.py
@@ -5,10 +5,8 @@ from mpl_toolkits.mplot3d import Axes3D
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-# Create a 3D array
-arr = [[[0]*1000 for i in range(1000)] for j in range (1000)]
-print("Welcome to 3D Mapping with Python where you can make a 3D map using simple x, y andd z co-ordinates ")
-print("Enter the co-ordinates x, y and z along with a value for the counter 'a' which should not be 1 until all the readings are over.")
+print("Welcome to 3D Mapping with Python where you can make a 3D map using simple x, y and z co-ordinates ")
+print("Enter the coordinates x, y, and z along with a value for the counter 'a' which should not be 1 until all the readings are over.")
 
 # input data
 a = 0
@@ -17,7 +15,6 @@ while(a!=1):
     y = int(input())
     z = int(input())
     a = int(input())
-    arr[x][y][z] = 1
     ax.scatter(x, y, z, color='black')
 
 plt.show()
